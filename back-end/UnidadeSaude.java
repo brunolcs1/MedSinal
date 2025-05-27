88bb3feb9fd9990d4f3ad56d92a13955c841dfb8
Paulo Henrique <145726119+PauloHenriquebds@users.noreply.github.com>
2025-05-27 16:21:14 -0300

    Update and rename .empyt to UnidadeSaude.java

---
+++ back-end/UnidadeSaude.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.*;
+
+@Entity
+public class UnidadeSaude {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private Double latitude;
+    private Double longitude;
+    private String horario;
+    private String telefone;
+
+    // Getters e setters omitidos para brevidade
+}
